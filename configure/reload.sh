009443a0d68feafdfd95dbffbccdeb70b2c88761
yaoxuzhao <yaoxuzhao@ledbrighter.com>
2017-11-02 12:20:54 +0800

    config

--- configure/reload.sh
+++ configure/reload.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export PATH=$PATH:/home/lepython/work/venv_py3/bin
 source /home/lepython/work/venv_py3/bin/activate
-/home/lepython/work/venv_py3/bin/uwsgi --ini `pwd`/runapp.ini
+/home/lepython/work/venv_py3/bin/uwsgi --reload `pwd`/run/bi_system.pid
--- configure/stopapp.sh
+++ configure/stopapp.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export PATH=$PATH:/home/lepython/work/venv_py3/bin
 source /home/lepython/work/venv_py3/bin/activate
-/home/lepython/work/venv_py3/bin/uwsgi --ini `pwd`/runapp.ini
+/home/lepython/work/venv_py3/bin/uwsgi --stop `pwd`/run/bi_system.pid

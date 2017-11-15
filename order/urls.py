37e22d3c28584b6b9dd5b8c135081946ffb965ae
姚许召 <yaoxuzhao@ledbrighter.com>
2017-11-15 11:39:09 +0800

    url

--- order/urls.py
+++ order/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^search_by_profile/$',views.search_by_profile,name='search_by_profile'),
     url(r'^search_by_name_and_profile/$',views.search_by_name_and_profile,name='search_by_name_and_profile'),
     url(r'^advise/$',views.add_advise,name='advise'),
+    url(r'^$',views.search_by_profile,name='index'),
 ]
